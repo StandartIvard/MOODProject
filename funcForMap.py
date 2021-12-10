1b7306c3846c2a754bc7012c95448c6b4ce54fc6
ivard <60704268+StandartIvard@users.noreply.github.com>
2021-12-10 21:00:20 +0300

    MainMenuClass added

--- funcForMap.py
+++ funcForMap.py
@@ -8,7 +8,3 @@ def translateMap(name):
         for j in range(len(map[i])):
             cords[i].append((int(map[i][j]), i, (len(map[i]) - j) - 1))
     return cords
-
-a = translateMap('mapName')
-for i in a:
-    print(i)
