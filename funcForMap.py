99b51a55a452382a7fa36eb24e12b30468fa8475
Ivanov Artem <ivard_iv@mail.ru>
2021-12-27 22:02:45 +0300

    funcForMap now works! added distance between every cord and camera

--- funcForMap.py
+++ funcForMap.py
@@ -1,4 +1,7 @@
-def translateMap(name):
+from py3d import polygon_center, dist
+
+
+def translateMap(name, cam, color):
     f = open('./data/maps/' + name + '.txt')
     map = f.readlines()
     cords = list()
@@ -8,20 +11,34 @@ def translateMap(name):
         map[i] = ''.join(map[i].split())
         map[i + 1] = ''.join(map[i + 1].split())
         for j in range(len(map[i]) - 1):
-            if map[i][j] == map[i][j + 1] == map[i + 1][j] == map[i + 1][j + 1]:
-                cords[i].append([(len(map[i]) - j - 1, int(map[i][j]), i), (len(map[i]) - j, int(map[i][j + 1]), i),
+            if map[i][j] == map[i][j + 1] == map[i + 1][j] == map[i + 1][j + 1] and int(map[i][j]) != 0:
+                temp = polygon_center([(len(map[i]) - j - 1, int(map[i][j]), i), (len(map[i]) - j, int(map[i][j + 1]), i),
                                 (len(map[i + 1]) - j, int(map[i + 1][j]), i + 1),
                                  (len(map[i + 1]) - j - 1, int(map[i + 1][j]), i + 1)])
-            elif map[i][j] != map[i + 1][j + 1]:
+                cords[i].append([[(len(map[i]) - j - 1, int(map[i][j]), i), (len(map[i]) - j, int(map[i][j + 1]), i),
+                                (len(map[i + 1]) - j, int(map[i + 1][j]), i + 1),
+                                 (len(map[i + 1]) - j - 1, int(map[i + 1][j]), i + 1)],
+                                 color, dist(temp, cam.pos)])
+            elif map[i][j] != map[i + 1][j + 1] and int(map[i][j]) != 0:
                 if map[i][j] == map[i][j + 1]:
-                    cords[i].append([(len(map[i]) - j - 1, int(map[i][j]), i), (len(map[i]) - j, int(map[i][j + 1]), i),
+                    temp = polygon_center([(len(map[i]) - j - 1, int(map[i][j]), i), (len(map[i]) - j, int(map[i][j + 1]), i),
                                      (len(map[i]) - j, 0, i), (len(map[i]) - j - 1, 0, i)])
+                    cords[i].append([[(len(map[i]) - j - 1, int(map[i][j]), i), (len(map[i]) - j, int(map[i][j + 1]), i),
+                                     (len(map[i]) - j, 0, i), (len(map[i]) - j - 1, 0, i)],
+                                     color, dist(temp, cam.pos)])
                 if map[i][j] == map[i + 1][j]:
-                    cords[i].append([(len(map[i]) - j - 1, int(map[i][j]), i), (len(map[i + 1]) - j, int(map[i][j]), i),
+                    temp = polygon_center([(len(map[i]) - j - 1, int(map[i][j]), i), (len(map[i + 1]) - j, int(map[i][j]), i),
                                      (len(map[i + 1]) - j, 0, i), (len(map[i]) - j - 1, 0, i)])
+                    cords[i].append([[(len(map[i]) - j - 1, int(map[i][j]), i), (len(map[i + 1]) - j, int(map[i][j]), i),
+                                     (len(map[i + 1]) - j, 0, i), (len(map[i]) - j - 1, 0, i)],
+                                     color, dist(temp, cam.pos)])
+    i = len(map) - 2
+    cords.append([])
+    for j in range(len(map[i]) - 1):
+        if map[i][j] == map[i][j + 1] and int(map[i][j]) != 0:
+            temp = polygon_center([(len(map[i]) - j - 1, int(map[i][j]), i), (len(map[i]) - j, int(map[i][j + 1]), i),
+                    (len(map[i]) - j, 0, i), (len(map[i]) - j - 1, 0, i)])
+            cords[i + 1].append([[(len(map[i]) - j - 1, int(map[i][j]), i), (len(map[i]) - j, int(map[i][j + 1]), i),
+                             (len(map[i]) - j, 0, i), (len(map[i]) - j - 1, 0, i)],
+                             color, dist(temp, cam.pos)])
     return cords
-
-
-a = translateMap('mapName')
-for i in a:
-    print(i)
